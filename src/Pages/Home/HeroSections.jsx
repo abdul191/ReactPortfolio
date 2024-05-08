aeb4c074de2c26ad84e1b938e1935f82947bb09e
Abdul Rehman <97659086+abdul191@users.noreply.github.com>
2024-05-08 22:07:26 +0500

    almost hero section style done

--- src/Pages/Home/HeroSections.jsx
+++ src/Pages/Home/HeroSections.jsx
@@ -1,9 +1,30 @@
 
 function HeroSections() {
   return (
-    <div>
-      <h1>Hero Section</h1>
+<section id="heroSection" className="heroSection">
+  <div className="heroSectionContentBox">
+    <div className="heroSectionContent">
+      <p className="sectionTitle">
+      Hey, {"I'm"} Abdul Rehman
+      </p>
+      <h1 className="heroSectionTitle">
+        <span className="heroSectionTitleColor">
+          Full Stack
+        </span> { " " }
+        <br />
+        Developer
+      </h1>
+      <p className="heroSectionDescription">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, repudiandae.
+        <br />Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit?
+      </p>
     </div>
+    <button className="btn btnPrimary">Get In Touch</button>
+  </div>
+  <div className="heroSectionImg">
+    <img src="../../../public/hero.jpg" alt="hero section" />
+  </div>
+</section>
   )
 }
 
