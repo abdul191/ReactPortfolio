0224ad8d0c425322ec066c339a97b2dab00bf599
Abdul Rehman <97659086+abdul191@users.noreply.github.com>
2024-05-09 19:07:01 +0500

    Navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import "./App.css"
-// import React from "react"
-// import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Home from "./Pages/Home/HomeScreen"
+import Navbar from "./Pages/Home/Navbar"
 function App() {
   return (
-    <>
-      <Home/>
-    </>
+    <div className="App">
+      <Router>
+        <div>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home/>}></Route>
+            <Route path="*" element={<h1>404  Not Found</h1>}></Route>
+          </Routes>
+        </div>
+      </Router>
+    </div>
   )
 }
 
---
+++ src/Pages/Home/Navbar.jsx
@@ -0,0 +1,117 @@
+import { useState, useEffect } from "react";
+import { Link } from "react-scroll";
+function Navbar() {
+  const [navActive, setNavActive] = useState(false);
+  const toggleNav = () => {
+    setNavActive(!navActive);
+  };
+  const closeMenu = () => {
+    setNavActive(false);
+  };
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth <= 500) {
+        closeMenu;
+      }
+    };
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+  useEffect(() => {
+    if (window.innerWidth <= 1200) {
+      closeMenu;
+    }
+  }, []);
+  return (
+    <>
+      <nav className={`navbar ${navActive ? "active" : " "}`}>
+        <div>
+          <img
+            src="../../../public/logo-apple-appstore-svgrepo-com.svg"
+            alt="logoipsum"
+            className="svg"
+          />
+        </div>
+        <a
+          href="#"
+          className={`navHamburger ${navActive ? "active" : " "}`}
+          onClick={toggleNav}>
+          <span className="navHamburgerLine"></span>
+          <span className="navHamburgerLine"></span>
+          <span className="navHamburgerLine"></span>
+        </a>
+        <div className={`navbarItems ${navActive ? "active" : " "}`}>
+          <ul>
+            <li>
+              <Link
+                onClick={closeMenu}
+                activeClass="navbarActiveContent"
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+                to="heroSection"
+                className="navbarContent">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                onClick={closeMenu}
+                activeClass="navbarActiveContent"
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+                to="MyPortfolio"
+                className="navbarContent">
+                Portfolio
+              </Link>
+            </li>
+            <li>
+              <Link
+                onClick={closeMenu}
+                activeClass="navbarActiveContent"
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+                to="AboutMe"
+                className="navbarContent">
+                About Me
+              </Link>
+            </li>
+            <li>
+              <Link
+                onClick={closeMenu}
+                activeClass="navbarActiveContent"
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+                to="Testimonials"
+                className="navbarContent">
+                Testimonials
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <Link
+          onClick={closeMenu}
+          activeClass="navbarActiveContent"
+          spy={true}
+          smooth={true}
+          offset={-70}
+          duration={500}
+          to="ContactMe"
+          className="btn btnOutlinePrimary">
+          Contact Me
+        </Link>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
