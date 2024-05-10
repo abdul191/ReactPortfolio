98944a318c994a718880284e46e486115e8eb646
Abdul Rehman <97659086+abdul191@users.noreply.github.com>
2024-05-10 23:24:20 +0500

    portfolio section added

--- src/Pages/Home/HomeScreen/index.jsx
+++ src/Pages/Home/HomeScreen/index.jsx
@@ -1,13 +1,14 @@
 import HeroSections from "../HeroSections";
 import Skills from "../Skills";
 import AboutMe from "../AboutMe";
-
+import Portfolio from "../Portfolio";
 function index() {
   return (
     <>
       <HeroSections />
       <Skills/>
       <AboutMe/>
+      <Portfolio/>
     </>
   );
 }
